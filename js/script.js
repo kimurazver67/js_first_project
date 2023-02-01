526f9fa1cc5cae607921b7bcbc76632d9f4f3890
Nikita Zverev <692623ak@gmail.com>
2023-02-01 16:06:33 +1000

    сделал автоматизацию процесса с задаванием вопросов + проверку на ошибкир

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@ const personalMovieDB = {
     private: false
 }
 
-const filmName = prompt('какой фильм ты смотрел последним?',''),
-      range = +prompt('Как ты его оценишь?',''),
-      filmName2 = prompt('какой фильм ты смотрел последним?',''),
-      range2 = +prompt('Как ты его оценишь?','');
 
-personalMovieDB.movies[filmName] = range;
-personalMovieDB.movies[filmName2] = range2;
+for (let i = 0; i<2 ; i++){
+    const filmName = prompt('какой фильм ты смотрел последним?',''),
+    range = +prompt('Как ты его оценишь?','');
+    if(filmName != null && range != null && filmName != "" && range != "" &&filmName.length < 50){
+        personalMovieDB.movies[filmName] = range;
+        console.log('done')
+    } else{
+        console.log('error');
+        i--;
+    }
+}
 
+if(personalMovieDB.count<10){
+    console.log('So small kinoman')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Classic')
+}else if(personalMovieDB.count >=30){
+    console.log('kinoman')
+} else{
+    console.log('Error')
+}
 console.log(personalMovieDB)
